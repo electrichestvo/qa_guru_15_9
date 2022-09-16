0a4ddfb31fc02f860f452919cbde2d820ec8531a
mmslotin <balu54@yandex.ru>
2022-09-16 19:07:17 +0300

    Add checking of fill form

--- src/test/java/com/demoqa/TestForm.java
+++ src/test/java/com/demoqa/TestForm.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -27,11 +28,11 @@ public class TestForm {
         $("[id=lastName]").setValue("Slotin");
         $("[id=userEmail]").setValue("hello@mars.com");
         $("[id=genterWrapper]").$(byText("Male")).click();
-        $("[id=userNumber]").setValue("799912345567");
+        $("[id=userNumber]").setValue("7999123455");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1988");
-        $(".react-datepicker__day--007:not(.react-datepicker__day--selected").click();
+        $(".react-datepicker__day--017:not(.react-datepicker__day--selected").click();
         $("[id=subjectsInput]").setValue("Biology").pressEnter();;
         $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFile(new File("src/test/resources/12345.png"));
@@ -41,5 +42,19 @@ public class TestForm {
         $(byText("Select City")).click();
         $(byText("Gurgaon")).click();
         $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+        $(".table-responsive").shouldHave(text("Misha Slotin"),
+                text("hello@mars.com"),
+                text("Male"),
+                text("7999123455"),
+                text("17 July,1988"),
+                text("Biology"),
+                text("Music"),
+                text("12345.png"),
+                text("SPB"),
+                text("NCR Gurgaon"));
+        $("#closeLargeModal").click();
     }
 }
